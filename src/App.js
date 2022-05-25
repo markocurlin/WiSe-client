c81a5ebced66a053dee32cbc94a3692674387176
markocurlin <marko.curlin22@gmail.com>
2022-05-25 17:01:31 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import TemperaturePage from './pages/TemperaturePage';
+import HumidityPage from './pages/HumidityPage';
+import LuxPage from './pages/LuxPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/temperature' element={<TemperaturePage />} />
+        <Route path='/humidity' element={<HumidityPage />} />
+        <Route path='/lux' element={<LuxPage />} />
+      </Routes>
+    </Router>  
   );
 }
 
